2c132f68c117b3f6b77860a8443b31bca21751c9
Samuel Kling <samtkling@gmail.com>
2018-10-24 22:50:31 -0400

    Activity box display phone screen like

---
+++ web/pt-app/src/app/activity-box/activity-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'activity-box',
+  templateUrl: './activity-box.component.html',
+  styleUrls: ['./activity-box.component.scss']
+})
+export class ActivityBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
